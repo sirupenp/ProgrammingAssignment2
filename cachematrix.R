497144e819ea4285fdd706f2e60b456f0b872402
Upen Patel <sirupenp@gmail.com>
2014-04-25 15:18:51 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -10,43 +10,43 @@
  makeCacheMatrix <- function(x = matrix()) {  
     m <- NULL  
     setMatrix <- function(y) {    
-      x <<- y    
-	  m <<- NULL  
+    x <<- y    
+    m <<- NULL  
     }
 	
-	#Get the value of matrix from cache  
-	getMatrix <- function() x
+#Get the value of matrix from cache  
+ getMatrix <- function() x
 	
-	#Set the value of inverse matrix into cache  
-	setInvMatrix <- function(inverseSet) m <<- inverseSet
+#Set the value of inverse matrix into cache  
+ setInvMatrix <- function(inverseSet) m <<- inverseSet
 	
-	 #Get the value of inverse matrix into cache  
-	 getInvMatrix <- function() m
+#Get the value of inverse matrix into cache  
+ getInvMatrix <- function() m
 	 
 	 
-	 #Output   
-	 list(setMatrix = setMatrix, getMatrix = getMatrix, 
-	     setInvMatrix = setInvMatrix,       
-		 getInvMatrix = getInvMatrix)
-	}
+#Output   
+ list(setMatrix = setMatrix, getMatrix = getMatrix, 
+     setInvMatrix = setInvMatrix,       
+     getInvMatrix = getInvMatrix)
+}
 
-	# cacheSolve is a function that accepts function list from makeCacheMatrix as
-	# parameter and checks if the inverse of the matrix already exits.
-	# It then returns the data from cache if it exists else it compute using "solve" function and
-	# caches using function setInvMatrix() defined in the makeCacheMatrix()
+# cacheSolve is a function that accepts function list from makeCacheMatrix as
+# parameter and checks if the inverse of the matrix already exits.
+# It then returns the data from cache if it exists else it compute using "solve" function and
+# caches using function setInvMatrix() defined in the makeCacheMatrix()
 	
-	cacheSolve <- function(x, ...) {  
-	m <- x$getInvMatrix()  
-	#Check to see if the matrix-inverse already exists using getInvMatrix(), if it does then return cached data  
-	if(!is.null(m)) {    
-	message("getting cached data")    
-	return(m)  
-	}
+cacheSolve <- function(x, ...) {  
+    m <- x$getInvMatrix()  
+  #Check to see if the matrix-inverse already exists using getInvMatrix(), if it does then return cached data  
+    if(!is.null(m)) {    
+    message("getting cached data")    
+    return(m)  
+    }
 	
-	#If the Inverse doesn't exist in cache then compute and store in cache using the setInvMatrix()  
-	data <- x$getMatrix()  
-	m <- solve(data, ...)  
-	x$setInvMatrix(m)  
-	m
+#If the Inverse doesn't exist in cache then compute and store in cache using the setInvMatrix()  
+  data <- x$getMatrix()  
+  m <- solve(data, ...)  
+  x$setInvMatrix(m)  
+  m
 	
-	}
+}
